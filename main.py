fe9a937ac3c2b061f6cfad43297b6bfa021f0e7e
Vapor8x <tencityy@gmail.com>
2025-01-12 12:35:14 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+
+#Python math
+print("Python Math by Vapor8x")
+
+operator = input("Enter a a operator + - * /: ")
+number1 = int(input("Enter your first number: "))
+number2 = int(input("Enter your second number: "))
+
+if operator == "+":
+    result = number1 + number2
+    print(f"Your Result is {result}")
+elif operator == "-":
+    result = number1 - number2
+    print(f"Your Result is {result}")
+elif operator == "*":
+    result = number1 * number2
+    print(f"Your Result is {result}")
+elif operator == "/":
+    result = number1 / number2
+    print(f"Your Result is {result}")
+
+
+#Thank you Message
+print("Thanks for Using comeback soon!")
+continue1 = input()
+exit(0)
